aaa95457e26b8334056f6d42068db26c165f7db3
anylots <842567494@qq.com>
2022-11-13 21:23:10 +0800

    para

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,59 @@
+use std::io::stdout;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::time::Duration;
+use std::time::{SystemTime, UNIX_EPOCH};
+use std::{string, thread};
 use webevm::model_info::AppInfo;
 use webevm::service::execute;
 
+static CURRENT_SEQ: AtomicUsize = AtomicUsize::new(0);
+
 fn main() {
+    let mut start = SystemTime::now();
+    let mut updated = false;
+
     println!("execute starting!");
-    let  uniswap: AppInfo = AppInfo {
+    let uniswap: AppInfo = AppInfo {
         name: String::from("uniswap"),
         app_type: String::from("dex"),
         tvl: 10000,
     };
-    let app_msg = execute::query_info(&uniswap);
-    println!("{}", app_msg);
+
+    // let app_msg = execute::query_info(&uniswap);
+
+    let count = 0;
+
+    for i in 0..1000 {
+        if (!updated) {
+            updated = true;
+            start = SystemTime::now();
+            println!("{}", "SystemTime updated");
+        }
+
+        thread::spawn(|| execute::query_info(&CURRENT_SEQ));
+
+        let count = CURRENT_SEQ.load(Ordering::Relaxed);
+        if (count % 100 == 0) {
+            let mut duration = SystemTime::now().duration_since(start).unwrap().as_millis();
+            if duration == 0 {
+                duration = 1;
+            }
+            println!(
+                "============> prove per second: {}",
+                count * 1000 / (duration as usize)
+            )
+        }
+    }
+    thread::sleep(Duration::from_millis(100));
+
+    let end = SystemTime::now();
+
+    println!("{}", CURRENT_SEQ.load(Ordering::Relaxed));
+
+    println!(
+        "done!start : {:?},end :{:?},duration:{:?}",
+        start,
+        end,
+        end.duration_since(start).unwrap().as_millis()
+    );
 }
--- src/service/execute.rs
+++ src/service/execute.rs
@@ -1,5 +1,12 @@
 use crate::model_info::app_info::AppInfo;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::{thread};
+use std::time::Duration;
 
-pub fn query_info(app: &AppInfo) -> String {
-    return serde_json::to_string(&app).unwrap();
+pub fn query_info(count: &AtomicUsize) {
+    // return serde_json::to_string(&app).unwrap();
+    count.fetch_add(1, Ordering::Relaxed);
+    thread::sleep(Duration::from_millis(10));
+
+    // println!("{}", "dosomthing");
 }

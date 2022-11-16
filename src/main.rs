fcdbe6a6a0f0f6feffb5ccbaf4297d7589bf02df
anylots <842567494@qq.com>
2022-11-16 20:46:48 +0800

    opt

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,13 @@
-use std::io::stdout;
 use std::process::{Command, Output};
-use std::str::Bytes;
-use std::sync::atomic::{AtomicUsize, Ordering};
-use std::time::{SystemTime, UNIX_EPOCH};
-use std::{string, thread};
-use webevm::model_info::AppInfo;
-use webevm::service::execute;
 use rand::{thread_rng, Rng};
-use tracing::{debug, error, info};
 
-static CURRENT_SEQ: AtomicUsize = AtomicUsize::new(0);
 
 fn main() {
-    let mut start = SystemTime::now();
-    let mut updated = false;
 
     println!("execute starting!");
 
     sout_prover_info();
 
-    {
-        // let uniswap: AppInfo = AppInfo {
-        //     name: String::from("uniswap"),
-        //     app_type: String::from("dex"),
-        //     tvl: 10000,
-        // };
-
-        // // let app_msg = execute::query_info(&uniswap);
-
-        // let count = 0;
-
-        // for i in 0..1000 {
-        //     if (!updated) {
-        //         updated = true;
-        //         // start = SystemTime::now();
-        //         println!("{}", "SystemTime updated");
-        //     }
-        //     execute::query_info(&CURRENT_SEQ, start);
-        //     // thread::spawn(move || execute::query_info(&CURRENT_SEQ, &start));
-
-        //     thread::sleep(Duration::from_millis(10));
-
-        //     let count = CURRENT_SEQ.load(Ordering::Relaxed);
-        //     if (count % 100 == 0) {
-        //         let mut duration = SystemTime::now().duration_since(start).unwrap().as_millis();
-        //         if duration == 0 {
-        //             duration = 1;
-        //         }
-        //         println!("{}", duration);
-        //         println!(
-        //             "============> prove per second: {}",
-        //             count * 1000 / (duration as usize)
-        //         )
-        //     }
-        // }
-        // thread::sleep(Duration::from_millis(100));
-
-        // let end = SystemTime::now();
-
-        // println!("{}", CURRENT_SEQ.load(Ordering::Relaxed));
-
-        // println!(
-        //     "done!start : {:?},end :{:?},duration:{:?}",
-        //     start,
-        //     end,
-        //     end.duration_since(start).unwrap().as_millis()
-        // );
-    }
 }
 
 fn sout_prover_info() {

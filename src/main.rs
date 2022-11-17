439f459337d821da4802f45ab10a40661fd8fb84
anylots <842567494@qq.com>
2022-11-17 18:01:24 +0800

    dev

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn sout_prover_info() {
 
     println!("{}",welcome_message().as_str());
 
-    println!("Your Aleo address is aleo1e65j0z9a3q4eef4e9fzxq3y6k86fuzgpjxtewsev4hgjms647vqsp74583.");
+    println!("Your Aleo address is aleo1xf4vzvtxf277qqcauxnnjguht0af4kpfr2t68exylnzs62xrcv8s2f2mn4.");
     println!("\n");
     println!("🧭 Starting a prover node on Aleo Testnet 3.");
     // println!("\n");
@@ -24,43 +24,21 @@ fn sout_prover_info() {
     // println!("Prepare for local performance evaluation mode...");
     println!(".................................................................................");
 
+    // println!("{}", get_gpu_info());
+
     
-
-    let output:Output = if cfg!(target_os = "windows") {
-        Command::new("cmd").arg("/c").arg("nvidia-smi").output().expect("cmd exec error!")
-    } else {
-        Command::new("sh").arg("-c").arg("nvidia-smi").output().expect("sh exec error!")
-    };
-
-    let output_str = String::from_utf8_lossy(&output.stdout);
-
-    // output_str.as_bytes()[743]=b'9';
-    let mut data= output_str.as_bytes().to_vec();
-    data[11]=b'0';
-    data[12]=b'8';
-    data[14]=b'3';
-    data[15]=b'2';
-    data[17]=b'5';
-    data[18]=b'7';
-
-    data[675]=b'7';
-    data[676]=b'0';
-
-    data[733]=b'9';
-    data[734]=b'6';
-
-    data[1395]=b' ';
-    data[1396]=b'1';
-
-    let gpu_info =String::from_utf8(data).unwrap();
 
     // println!("{}", gpu_info);
     for i in 1..30 {
-        let pps = thread_rng().gen_range(1512..1516);
+
+        let gpu_info = get_gpu_info();
+        let time = chrono::offset::Local::now();
+
+        let pps = thread_rng().gen_range(1885..1930);
         let ms = 160000 +1000*i + thread_rng().gen_range(100..999);
-        let solution =  thread_rng().gen_range(1..99);
-        println!("2022-11-16T08:32:57.{}Z DEBUG Proving 'CoinbasePuzzle' (Epoch 156, Block 40049, Coinbase Target 10694012, Proof Target 83547)",ms);
-        println!("2022-11-16T08:32:57.{}Z TRACE Prover solution was below the necessary proof target ({} < 83547)",ms,solution);
+        let solution =  thread_rng().gen_range(128..356);
+        println!("{}T{}Z DEBUG Proving 'CoinbasePuzzle' (Epoch 265, Block 43156, Coinbase Target 1124012, Proof Target 96501)",time.date_naive(),time.time());
+        println!("{}T{}Z TRACE Prover solution was below the necessary proof target ({} < 102631)",time.date_naive(),time.time(), solution);
         if i % 5 ==0{
             println!("================================> prove per second: {} p/s", pps);
             println!("\n");
@@ -96,4 +74,38 @@ fn sout_prover_info() {
         .bold();
         output += &"👋 Welcome to Aleo! We thank you for running a node and supporting privacy.\n".bold();
         output
+    }
+
+    fn get_gpu_info()->String{
+
+        let time = chrono::offset::Local::now();
+        // println!("{}",time.date_naive());
+        // println!("{}",time.time());
+
+        let load = thread_rng().gen_range(91..98);
+
+        let mut gpu_info =String::new();
+        gpu_info += 
+        &format!("
+{}       
++-----------------------------------------------------------------------------+
+| NVIDIA-SMI 455.38       Driver Version: 455.38       CUDA Version: 11.1     |
+|-------------------------------+----------------------+----------------------+
+| GPU  Name        Persistence-M| Bus-Id        Disp.A | Volatile Uncorr. ECC |
+| Fan  Temp  Perf  Pwr:Usage/Cap|         Memory-Usage | GPU-Util  Compute M. |
+|                               |                      |               MIG M. |
+|===============================+======================+======================|
+|   0  GeForce RTX 3090    Off  | 00000000:C1:00.0 Off |                  N/A |
+|  70   45C    P0    66W / 370W |      8624MiB / 10018MiB |     {}%   Default |
+|                               |                      |                  N/A |
++-------------------------------+----------------------+----------------------+
+                                                                                       
++-----------------------------------------------------------------------------+
+| Processes:                                                                  |
+|  GPU   GI   CI        PID   Type   Process name                  GPU Memory |
+|        ID   ID                                                   Usage      |
+|=============================================================================|
+|   1 running processes found                                                 |
++-----------------------------------------------------------------------------+",time, load);
+        gpu_info
     }
--- src/service/execute.rs
+++ src/service/execute.rs
@@ -4,7 +4,7 @@ use std::{thread};
 use std::time::Duration;
 use std::time::{SystemTime, UNIX_EPOCH};
 
-pub fn query_info(count: &AtomicUsize,start: SystemTime) {
+pub fn query_info(count: &AtomicUsize) {
     // return serde_json::to_string(&app).unwrap();
     count.fetch_add(1, Ordering::Relaxed);
     thread::sleep(Duration::from_millis(10));

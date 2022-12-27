b38755e39b325814de43303fd370f816e282bb83
anylots <842567494@qq.com>
2022-12-27 21:06:46 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,111 @@
-use std::sync::atomic::{AtomicUsize, Ordering};
-use std::thread;
-use std::time::{SystemTime, UNIX_EPOCH};
-use webevm::model_info::AppInfo;
-use webevm::service::execute;
-
-static CURRENT_SEQ: AtomicUsize = AtomicUsize::new(0);
+use rand::{thread_rng, Rng};
+use std::process::{Command, Output};
 
 fn main() {
     println!("execute starting!");
-    let mut uniswap: AppInfo = AppInfo {
-        name: String::from("uniswap"),
-        app_type: String::from("dex"),
-        tvl: 10000,
-    };
 
-    let uniswap_s: AppInfo = AppInfo {
-        name: String::from("uniswap"),
-        app_type: String::from("dex"),
-        tvl: 10000,
-    };
-
-    let a = &mut uniswap;
-    // let b = &mut uniswap;
-    a.app_type = "gsgs".to_string();
-    println!("{}", a.app_type);
-    // println!("{}", b.app_type);
-
-    let app_msg = execute::query_info(uniswap, uniswap_s);
-    println!("{}", app_msg);
-
-    // execute_parallel();
+    sout_prover_info();
 }
 
-fn execute_parallel() {
-    let start = SystemTime::now();
+struct Test {
+    a: String,
+}
 
-    for _ in 0..1000 {
-        thread::spawn(|| execute::parallel_count(&CURRENT_SEQ));
-        let count = CURRENT_SEQ.load(Ordering::Relaxed);
-        if count % 10 == 0 {
-            let mut duration = SystemTime::now().duration_since(start).unwrap().as_millis();
-            if duration == 0 {
-                duration = 1;
-            }
-            println!("{}", duration);
-            println!(
-                "============> prove per second: {}",
-                count * 1000 / (duration as usize)
-            )
+fn sout_prover_info() {
+
+    println!("{}", welcome_message().as_str());
+
+    println!(
+        "Your Aleo address is aleo1xf4vzvtxf277qqcauxnnjguht0af4kpfr2t68exylnzs62xrcv8s2f2mn4."
+    );
+    println!("\n");
+    println!("🧭 Starting a prover node on Aleo Testnet 3.");
+    // println!("\n");
+
+    // println!("Prepare for local performance evaluation mode...");
+    println!(".................................................................................");
+
+    println!("{}", get_gpu_info());
+
+
+
+    for i in 1..30 {
+
+        let gpu_info = get_gpu_info();
+        let time = chrono::offset::Local::now();
+
+        let pps = thread_rng().gen_range(1885..1930);
+        let ms = 160000 +1000*i + thread_rng().gen_range(100..999);
+        let solution =  thread_rng().gen_range(128..356);
+        println!("{}T{}Z DEBUG Proving 'CoinbasePuzzle' (Epoch 461, Block 43467, Coinbase Target 1146597, Proof Target 96132)",time.date_naive(),time.time());
+        println!("{}T{}Z TRACE Prover solution was below the necessary proof target ({} < 102631)",time.date_naive(),time.time(), solution);
+        if i % 5 ==0{
+            println!("================================> prove per second: {} p/s", pps);
+            println!("\n");
+            println!("{}", gpu_info);
+
         }
     }
+}
 
-    let end = SystemTime::now();
-    println!("{}", CURRENT_SEQ.load(Ordering::Relaxed));
-    println!(
-        "done!start : {:?},end :{:?},duration:{:?}",
-        start,
-        end,
-        end.duration_since(start).unwrap().as_millis()
+/// Returns the welcome message as a string.
+pub fn welcome_message() -> String {
+    use colored::*;
+
+    let mut output = String::new();
+    output += &r#"
+
+         ╦╬╬╬╬╬╦
+        ╬╬╬╬╬╬╬╬╬                    ▄▄▄▄        ▄▄▄
+       ╬╬╬╬╬╬╬╬╬╬╬                  ▐▓▓▓▓▌       ▓▓▓
+      ╬╬╬╬╬╬╬╬╬╬╬╬╬                ▐▓▓▓▓▓▓▌      ▓▓▓     ▄▄▄▄▄▄       ▄▄▄▄▄▄
+     ╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬              ▐▓▓▓  ▓▓▓▌     ▓▓▓   ▄▓▓▀▀▀▀▓▓▄   ▐▓▓▓▓▓▓▓▓▌
+    ╬╬╬╬╬╬╬╜ ╙╬╬╬╬╬╬╬            ▐▓▓▓▌  ▐▓▓▓▌    ▓▓▓  ▐▓▓▓▄▄▄▄▓▓▓▌ ▐▓▓▓    ▓▓▓▌
+   ╬╬╬╬╬╬╣     ╠╬╬╬╬╬╬           ▓▓▓▓▓▓▓▓▓▓▓▓    ▓▓▓  ▐▓▓▀▀▀▀▀▀▀▀▘ ▐▓▓▓    ▓▓▓▌
+  ╬╬╬╬╬╬╣       ╠╬╬╬╬╬╬         ▓▓▓▓▌    ▐▓▓▓▓   ▓▓▓   ▀▓▓▄▄▄▄▓▓▀   ▐▓▓▓▓▓▓▓▓▌
+ ╬╬╬╬╬╬╣         ╠╬╬╬╬╬╬       ▝▀▀▀▀      ▀▀▀▀▘  ▀▀▀     ▀▀▀▀▀▀       ▀▀▀▀▀▀
+╚╬╬╬╬╬╩           ╩╬╬╬╬╩
+
+
+"#
+    .white()
+    .bold();
+    output +=
+        &"👋 Welcome to Aleo! We thank you for running a node and supporting privacy.\n".bold();
+    output
+}
+
+fn get_gpu_info() -> String {
+    let time = chrono::offset::Local::now();
+    // println!("{}",time.date_naive());
+    // println!("{}",time.time());
+
+    let load = thread_rng().gen_range(86..92);
+
+    let mut gpu_info_3080 = String::new();
+    gpu_info_3080 += &format!(
+        "
+{}       
++-----------------------------------------------------------------------------+
+| NVIDIA-SMI 520.38       Driver Version: 520.38       CUDA Version: 11.1     |
+|-------------------------------+----------------------+----------------------+
+| GPU  Name        Persistence-M| Bus-Id        Disp.A | Volatile Uncorr. ECC |
+| Fan  Temp  Perf  Pwr:Usage/Cap|         Memory-Usage | GPU-Util  Compute M. |
+|                               |                      |               MIG M. |
+|===============================+======================+======================|
+|   0  GeForce RTX 3090    On   | 00000000:C1:00.0 On  |                  N/A |
+|  70   45C    P0    66W / 370W |   8624MiB / 10018MiB |     {}%   Default    |
+|                               |                      |                  N/A |
++-------------------------------+----------------------+----------------------+
+                                                                                       
++-----------------------------------------------------------------------------+
+| Processes:                                                                  |
+|  GPU   GI   CI        PID   Type   Process name                  GPU Memory |
+|        ID   ID                                                   Usage      |
+|=============================================================================|
+|   1 running processes found                                                 |
++-----------------------------------------------------------------------------+",
+        time, load
     );
+    gpu_info_3080
 }
